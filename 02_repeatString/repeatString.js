733cb853e6964f37a101d270b8284fe72879b41d
hukabo <dlwlsdn153@gmail.com>
2024-01-24 08:49:38 +0900

    javascript02 ans

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let mergedString = "";
+    if(num < 0) {
+        return "ERROR";
+    } 
+    // else {
+    //     for(let i = 0; i < num; i++) {
+    //         mergedString = mergedString + string;
+    //     }
+    //     return mergedString;
+    // }
+    return string.repeat(num);
 };
 
 // Do not edit below this line
