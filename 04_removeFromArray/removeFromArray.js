e36bb9fe120ce6235a8e3bbbf733a01522376a50
hukabo <dlwlsdn153@gmail.com>
2024-01-24 21:07:34 +0900

    js04 ans

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,33 @@
-const removeFromArray = function() {
+// const removeFromArray = function() {
+//     const arr = arguments[0];
 
-};
+//     for (let i = 1; i < arguments.length; i++) {
+//         const index = arr.indexOf(arguments[i]);
+//         if(index !== -1){
+//             arr.splice(index, 1);
+//         }
+//     }
+
+//     return arr;
+// };
+
+// const removeFromArray = function() {
+//     let arr = arguments[0];
+    
+//     for(let i = 1; i < arguments.length; i++){
+//         arr = arr.filter((element) => element !== arguments[i]);
+//     }
+    
+//     return arr;
+// }
+
+const removeFromArray = function(arr) {
+    const elementToRemove = Array.from(arguments).slice(1);
+
+    const removedArr = arr.filter(element => !elementToRemove.includes(element))
+
+    return removedArr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
