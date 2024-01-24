6d64a77ba9b4afddfd16ef38878041a959114865
hukabo <dlwlsdn153@gmail.com>
2024-01-24 21:08:44 +0900

    js07 ans

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  fahrenheit = (fahrenheit - 32) * 5 / 9;
+  const result = Math.round(fahrenheit * 10) / 10;
+
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  celsius = (celsius * (9 / 5) + 32);
+  const result = Math.round(celsius * 10) / 10;
+
+  return result;
 };
 
 // Do not edit below this line
