1895cefc36aae49bc56e8060a28018b61cc0c5c9
hukabo <dlwlsdn153@gmail.com>
2024-02-04 06:29:38 +0900

    js08 ans

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = arr.reduce((sum, number) => {
+    return sum + number;
+  }, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((mul, number) => {
+    return mul * number;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+	for(let i = 1; i <= number; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
