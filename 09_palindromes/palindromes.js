2666b5044507ae3d50c818951e48bdd5baa73737
hukabo <dlwlsdn153@gmail.com>
2024-02-04 06:30:00 +0900

    js09 ans

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let strArr = str.toLowerCase().split('');
+    strArr = strArr.filter(char => (char !== '!' && char !== ' ' && char !== '.' && char !== ','));
+    // for (let i = strArr.length; i >= 0; i--) {
+    //     if(strArr[i] === '!' ||
+    //        strArr[i] === ' ' ||
+    //        strArr[i] === '.' ||
+    //        strArr[i] === ',') {
+    //         strArr.splice(i,1);
+    //        };
+    // }
+    
+    let reversed = strArr.slice().reverse();
 
+        if(strArr.toString() === reversed.toString()) {
+            return true;
+        }
+        return false;
 };
 
 // Do not edit below this line
