333b146d323087c4239cba19f6355fda8c9acf0f
hukabo <dlwlsdn153@gmail.com>
2024-02-04 08:57:58 +0900

    js10 ans

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let num = Number(number);
+    if(num < 0) {
+        return "OOPS";
+    } else if(num === 0) {
+        return 0;
+    }
+    let arr = [0,1];
 
+    for (let i = 2; i <= num; i++) {
+        arr.push(arr[i-1] + arr[i-2]);
+    };
+
+    return arr[arr.length - 1];
 };
 
 // Do not edit below this line
